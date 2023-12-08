6cf012263e97dbe6a9673a06605039df65abc34c
Henrik Falch <henrik.falch@finn.no>
2023-12-08 10:49:11 +0100

    day 2 init

---
+++ src/main/kotlin/no/finntech/aoc2023/Day2.kt
@@ -0,0 +1,17 @@
+package no.finntech.aoc2023
+
+class Day2 {
+
+//    fun getAnswer1(): Int {
+//        return fileLinesAsList("aoc2023/day1.txt").sumOf { calibrationValue(it) }
+//    }
+//
+//    fun getAnswer2(): Int {
+//        return fileLinesAsList("aoc2023/day1.txt").sumOf { calibrationValueWithNumberStrings(it) }
+//    }
+}
+
+fun main() {
+//    println("Answer1: ${Day2().getAnswer1()}")
+//    println("Answer2: ${Day2().getAnswer2()}")
+}
